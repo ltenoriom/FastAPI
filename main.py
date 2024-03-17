4a45b8170b4447514e4177cfad476f8ba7f4ad73
Luis Fernando Tenorio Mora <142266185+ltenoriom@users.noreply.github.com>
2024-03-17 11:56:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 from fastapi import FastAPI
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+
+
+def send_email(subject, message, to_address):
+    from_address = 'your_outlook_email@example.com'
+    password = os.getenv("OUTLOOK_EMAIL_PASS")  # Make sure you have this environment variable set
+    msg = MIMEMultipart()
+    msg['From'] = "SmartBids.ai - Email verification <" + from_address + ">"
+    msg['To'] = to_address
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'html'))
+
+    # Connect to Outlook's SMTP server
+    server = smtplib.SMTP('smtp.office365.com', 587)
+    server.starttls()  # Enable TLS encryption
+
+    # Login to your Outlook account
+    server.login(from_address, password)
+
+    # Send the email
+    server.sendmail(from_address, to_address, msg.as_string())
+
+    # Close the connection
+    server.quit()
